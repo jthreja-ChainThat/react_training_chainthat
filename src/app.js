808fd84ba06e2b814f2eb419de108c292e3f3082
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-07-30 15:40:30 +0530

    completed todo application

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import "./todo.css";
 class App extends PureComponent {
   state = {
     todoList: [],
+    filter: "all",
   };
 
   inputRef = createRef();
@@ -43,41 +44,83 @@ class App extends PureComponent {
     });
   };
 
+  deleteTodo = (todo) => {
+    this.setState(({ todoList }) => {
+      const index = todoList.findIndex((item) => item.id === todo.id);
+      return {
+        todoList: [...todoList.slice(0, index), ...todoList.slice(index + 1)],
+      };
+    });
+  };
+
   render() {
     console.log("render");
-    const { todoText, todoList } = this.state;
+    const { filter, todoList } = this.state;
     return (
-      <>
+      <div className="container">
         <h1>Todo App</h1>
 
         <form onSubmit={this.addTodo}>
           <input type="text" ref={this.inputRef} />
           <button type="submit">Add Todo</button>
         </form>
-        <div>
-          {todoList.map((todo, index) => {
-            return (
-              <div key={todo.id} className="todo-list-wrapper">
-                <input
-                  type="checkbox"
-                  name="isDone"
-                  id="isDone"
-                  checked={todo.isDone}
-                  onChange={() => this.toggleTodo(todo)}
-                />
-                <span
-                  style={{
-                    textDecoration: todo.isDone ? "line-through" : "none",
-                  }}
-                >
-                  {todo.text}
-                </span>
-                <button type="button">Delete Todo</button>
-              </div>
-            );
-          })}
+        <div className="todo-list-container">
+          {todoList
+            .filter((todo) => {
+              switch (filter) {
+                case "pending":
+                  return !todo.isDone;
+                case "completed":
+                  return todo.isDone;
+                default:
+                  return true;
+              }
+            })
+            .map((todo, index) => {
+              return (
+                <div key={todo.id} className="todo-list-wrapper">
+                  <input
+                    type="checkbox"
+                    name="isDone"
+                    id="isDone"
+                    checked={todo.isDone}
+                    onChange={() => this.toggleTodo(todo)}
+                  />
+                  <span
+                    style={{
+                      textDecoration: todo.isDone ? "line-through" : "none",
+                    }}
+                  >
+                    {todo.text}
+                  </span>
+                  <button type="button" onClick={() => this.deleteTodo(todo)}>
+                    Delete Todo
+                  </button>
+                </div>
+              );
+            })}
         </div>
-      </>
+        <div className="filter-button-wrapper">
+          <button
+            type="button"
+            onClick={() => this.setState({ filter: "all" })}
+          >
+            All
+          </button>
+          <button
+            type="button"
+            onClick={() => this.setState({ filter: "pending" })}
+          >
+            Pending
+          </button>
+          <button
+            type="button"
+            onClick={() => this.setState({ filter: "completed" })}
+          >
+            Completed
+          </button>
+        </div>
+      </div>
     );
   }
 }
