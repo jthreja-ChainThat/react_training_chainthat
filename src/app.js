6a0ec314ef00e96539b3f68c57ef7b136d505577
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-08-02 11:08:44 +0530

    commit code

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { PureComponent, createRef } from "react";
 import TodoList from "./todoList";
 import TodoForm from "./todoForm";
 import TodoFilter from "./todoFilter";
+import { v4 as uuidv4 } from "uuid";
 import axiosInstance from "./utils/axiosInstance";
 import "./todo.css";
 
@@ -9,7 +10,7 @@ class App extends PureComponent {
   state = {
     todoList: [],
     filter: "all",
-    status: "idle",
+    status: [],
   };
 
   inputRef = createRef();
@@ -19,33 +20,56 @@ class App extends PureComponent {
   }
 
   fetchData = async () => {
+    const uniqueId = uuidv4();
     try {
-      this.setState({ status: "load_data" });
+      this.setState(({ status }) => ({
+        status: [...status, { id: uniqueId, name: "load_data_request" }],
+      }));
       const res = await axiosInstance.get("todoList");
-      this.setState({ status: "load_data_request" });
-      this.setState({ todoList: res.data, loading: false, status: "idle" });
+      this.setState(({ status }) => {
+        const updatedStatusList = status.filter((x) => x.id !== uniqueId);
+        return {
+          todoList: res.data,
+          loading: false,
+          status: updatedStatusList,
+        };
+      });
     } catch (error) {
       console.log(error.message);
-      this.setState({ status: "load_data_error" });
+      this.setState(({ status }) => {
+        const updatedStatusList = status.map((x) => {
+          if (x.id === uniqueId) {
+            return { ...x, name: "load_data_error" };
+          }
+          return x;
+        });
+
+        return { status: updatedStatusList };
+      });
     }
   };
 
   addTodo = async (event) => {
+    const uniqueId = uuidv4();
     try {
       event.preventDefault();
 
-      this.setState({ status: "add_data" });
+      this.setState(({ status }) => ({
+        status: [...status, { id: uniqueId, name: "add_data" }],
+      }));
+
       const res = await axiosInstance.post("todoList", {
         text: this.inputRef.current.value,
         isDone: false,
       });
 
       this.setState(
-        ({ todoList, todoText }) => {
+        ({ todoList, status }) => {
+          const updatedStatusList = status.filter((x) => x.id !== uniqueId);
           return {
             todoList: [...todoList, res.data],
             filter: "all",
-            status: "idle",
+            status: updatedStatusList,
           };
         },
         () => {
@@ -53,48 +77,105 @@ class App extends PureComponent {
         }
       );
     } catch (error) {
-      this.setState({ status: "add_data_error" });
+      this.setState(({ status }) => {
+        const updatedStatusList = status.map((x) => {
+          if (x.id === uniqueId) {
+            return { ...x, name: "add_data_error" };
+          }
+          return x;
+        });
+
+        return { status: updatedStatusList };
+      });
     }
   };
 
   toggleTodo = async (todo) => {
+    const uniqueId = uuidv4();
     try {
-      this.setState({ status: "update_data" });
-      const res = await axiosInstance.put(
-        `todoList/${todo.id}`,
-        { ...todo, isDone: !todo.isDone }
-      );
+      this.setState(({ status }) => {
+        let updatedStatus = status;
 
-      this.setState(({ todoList }) => {
+        const index = status.findIndex((x) => x.statusId === todo.id);
+
+        if (index === -1) {
+          updatedStatus = [
+            ...status,
+            { id: uniqueId, name: "update_data", statusId: todo.id },
+          ];
+        } else {
+          updatedStatus = [
+            ...status.slice(0, index),
+            { ...status[index], name: "update_data" },
+            ...status.slice(index + 1),
+          ];
+        }
+        return {
+          status: updatedStatus,
+        };
+      });
+
+      const res = await axiosInstance.put(`todoList/${todo.id}`, {
+        ...todo,
+        isDone: !todo.isDone,
+      });
+
+      this.setState(({ todoList, status }) => {
         const index = todoList.findIndex((item) => item.id === todo.id);
+        const updatedStatusList = status.filter((x) => x.id !== uniqueId);
         return {
           todoList: [
             ...todoList.slice(0, index),
             res.data,
             ...todoList.slice(index + 1),
           ],
-          status: "idle",
+          status: updatedStatusList,
         };
       });
     } catch (error) {
-      this.setState({ status: "update_data_error" });
+      this.setState(({ status }) => {
+        const updatedStatusList = status.map((x) => {
+          if (x.id === uniqueId) {
+            return { ...x, name: "update_data_error" };
+          }
+          return x;
+        });
+
+        return { status: updatedStatusList };
+      });
     }
   };
 
   deleteTodo = async (todo) => {
+    const uniqueId = uuidv4();
     try {
-      this.setState({ status: "delete_data" });
+      this.setState(({ status }) => ({
+        status: [
+          ...status,
+          { id: uniqueId, name: "delete_data", statusId: todo.id },
+        ],
+      }));
       await axiosInstance.delete(`todoList/${todo.id}`);
 
-      this.setState(({ todoList }) => {
+      this.setState(({ todoList, status }) => {
         const index = todoList.findIndex((item) => item.id === todo.id);
+        const updatedStatusList = status.filter((x) => x.id !== uniqueId);
         return {
           todoList: [...todoList.slice(0, index), ...todoList.slice(index + 1)],
-          status: "idle",
+          status: updatedStatusList,
         };
       });
     } catch (error) {
-      this.setState({ status: "delete_data_error" });
+      this.setState(({ status }) => {
+        const updatedStatusList = status.map((x) => {
+          if (x.id === uniqueId) {
+            return { ...x, name: "delete_data_error" };
+          }
+          return x;
+        });
+
+        return { status: updatedStatusList };
+      });
     }
   };
 
@@ -111,11 +192,11 @@ class App extends PureComponent {
       }
     });
 
-    if (status === "load_data") {
+    if (status.some((x) => x.name === "load_data")) {
       return <h1>Loading...</h1>;
     }
 
-    if (status.includes("error")) {
+    if (status.some((x) => x.name.includes("load_data_error"))) {
       return (
         <div>
           <h1>Try After Sometime!</h1>
--- src/todoList.js
+++ src/todoList.js
@@ -2,33 +2,47 @@ import React, { memo } from "react";
 
 const TodoList = ({ todoList, toggleTodo, deleteTodo, status }) => {
   console.log("TodoList");
+  console.log(status);
+
   return (
     <div className="todo-list-container">
-      {todoList.map((todo, index) => {
+      {todoList.map((todo) => {
         return (
-          <div key={todo.id} className="todo-list-wrapper">
-            <input
-              type="checkbox"
-              name="isDone"
-              id="isDone"
-              checked={todo.isDone}
-              disabled={status === "update_data"}
-              onChange={() => toggleTodo(todo)}
-            />
-            <span
-              style={{
-                textDecoration: todo.isDone ? "line-through" : "none",
-              }}
-            >
-              {todo.text}
-            </span>
-            <button
-              type="button"
-              disabled={status === "delete_data"}
-              onClick={() => deleteTodo(todo)}
-            >
-              Delete Todo
-            </button>
+          <div key={todo.id}>
+            <div className="todo-list-wrapper">
+              <input
+                type="checkbox"
+                name="isDone"
+                id="isDone"
+                checked={todo.isDone}
+                disabled={status.some(
+                  (x) => x.name === "update_data" && x.statusId === todo.id
+                )}
+                onChange={() => toggleTodo(todo)}
+              />
+              <span
+                style={{
+                  textDecoration: todo.isDone ? "line-through" : "none",
+                }}
+              >
+                {todo.text}
+              </span>
+              <button
+                type="button"
+                disabled={status.some(
+                  (x) => x.name === "delete_data" && x.statusId === todo.id
+                )}
+                onClick={() => deleteTodo(todo)}
+              >
+                Delete Todo
+              </button>
+            </div>
+            {status.some(
+              (x) =>
+                (x.name === "delete_data_error" ||
+                  x.name === "update_data_error") &&
+                x.statusId === todo.id
+            ) && <p>record is not deleted</p>}
           </div>
         );
       })}
